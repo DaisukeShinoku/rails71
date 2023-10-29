2b71097a6d37b97d6d833c948f6a897cabd73400
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2023-10-29 09:38:02 +0900

    teamのE2Eテスト一連の流れ

--- spec/system/teams_spec.rb
+++ spec/system/teams_spec.rb
@@ -47,4 +47,31 @@ RSpec.describe "Teams", type: :system do
       end
     end
   end
+
+  scenario "一連のteam操作を行う" do
+    # チーム一覧ページに遷移する
+    visit teams_url
+    expect(page).to have_selector "div", text: "チームが登録されていません。", exact_text: true
+    # チーム新規登録ページに遷移する
+    click_link "参加チーム新規登録"
+    # チームを新規登録する
+    fill_in "Name", with: "Systemチーム1"
+    click_button "Create Team"
+    expect(page).to have_selector "h1", text: "Systemチーム1の選手一覧", exact_text: true
+    # チーム一覧ページに遷移する
+    click_link "別チームの情報を見る"
+    expect(page).to have_selector "li", text: "Systemチーム1", exact_text: true
+    # チームを新規登録する
+    click_link "参加チーム新規登録"
+    fill_in "Name", with: "Systemチーム2"
+    click_button "Create Team"
+    expect(page).to have_selector "h1", text: "Systemチーム2の選手一覧", exact_text: true
+    # チーム一覧ページに遷移する
+    click_link "別チームの情報を見る"
+    expect(page).to have_selector "li", text: "Systemチーム1", exact_text: true
+    expect(page).to have_selector "li", text: "Systemチーム2", exact_text: true
+    # チーム詳細ページに遷移する
+    click_link "Systemチーム2"
+    expect(page).to have_selector "h1", text: "Systemチーム2の選手一覧", exact_text: true
+  end
 end
